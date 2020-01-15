1711e097aaf2625dfde8565d4497ad91c33e9606
forrestlin <forrest_lin@htc.com>
2020-01-15 15:42:47 +0800

    error handling

--- scripts/label-append.py
+++ scripts/label-append.py
@@ -46,22 +46,22 @@ def main():
             if (data['kind'] == "Deployment") or \
                (data['kind'] == "DaemonSet"):
                 # add label: tag
-                data['spec']['template']['metadata']['labels']['tag'] = tag[60:]
+                data['spec']['template']['metadata']['labels']['tag'] = tag[-63:]
                 # add label: imageName, imageVersion
                 image_ls = [c['image'] for c in data['spec']['template']['spec']['containers']]
-                image_name = '_'.join(i.split(':')[0][i.split(':')[0].rfind('/')+1:][-16:] for i in image_ls)    # remove all chars before the last '/', and
-                image_version = '_'.join(i.split(':')[1][-16:] for i in image_ls)                                # each was truncated to last 16 chars
-                data['spec']['template']['metadata']['labels']['imageName'] = image_name
+                image_name = 'x_' + '_'.join(i.split(':')[0][i.split(':')[0].rfind('/')+1:][-16:] for i in image_ls)           # remove all chars before the last '/', and
+                image_version = 'x_' + '_'.join(i.split(':')[1][-16:] for i in image_ls)                                       # each was truncated to last 16 chars, and
+                data['spec']['template']['metadata']['labels']['imageName'] = image_name                                       # starts with 'x_'
                 data['spec']['template']['metadata']['labels']['imageVersion'] = image_version
                 result.append(data)
             elif data['kind'] == "CronJob":
                 # add label: tag
-                data['spec']['jobTemplate']['spec']['template']['metadata']['labels']['tag'] = tag[60:]
+                data['spec']['jobTemplate']['spec']['template']['metadata']['labels']['tag'] = tag[-63:]
                 # add label: imageName, imageVersion
                 image_ls = [c['image'] for c in data['spec']['jobTemplate']['spec']['template']['spec']['containers']]
-                image_name = '_'.join(i.split(':')[0][i.split(':')[0].rfind('/')+1:][-16:] for i in image_ls)    # remove all chars before the last '/', and
-                image_version = '_'.join(i.split(':')[1][-16:] for i in image_ls)                                # each was truncated to last 16 chars
-                data['spec']['jobTemplate']['spec']['template']['metadata']['labels']['imageName'] = image_name
+                image_name = 'x_' + '_'.join(i.split(':')[0][i.split(':')[0].rfind('/')+1:][-16:] for i in image_ls)           # remove all chars before the last '/', and
+                image_version = 'x_' + '_'.join(i.split(':')[1][-16:] for i in image_ls)                                       # each was truncated to last 16 chars
+                data['spec']['jobTemplate']['spec']['template']['metadata']['labels']['imageName'] = image_name                # starts with 'x_'
                 data['spec']['jobTemplate']['spec']['template']['metadata']['labels']['imageVersion'] = image_version
                 result.append(data)
             elif 'kind' in data.keys():
